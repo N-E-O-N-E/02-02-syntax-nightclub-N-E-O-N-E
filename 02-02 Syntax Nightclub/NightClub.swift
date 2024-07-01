850556009a2c10998f43abe077f3ea6a69379f93
Markus Wirtz <info@markuswirtz.net>
2024-07-01 13:52:39 +0200

    Alles Aufgaben - Check

---
+++ 02-02 Syntax Nightclub/NightClub.swift
@@ -0,0 +1,25 @@
+//
+//  NightClub.swift
+//  02-02 Syntax Nightclub
+//
+//  Created by Markus Wirtz on 01.07.24.
+//
+import Foundation
+
+// 1.2 NightClub Status
+
+var clubName: String = "NumberOneClub"
+var isOpen: Bool = true
+var maxGuests: Int = 200
+var aktGuests: Int = 167
+
+
+
+
+
+
+
+
+
+
+
--- 02-02 Syntax Nightclub/main.swift
+++ 02-02 Syntax Nightclub/main.swift
@@ -1,1 +1,43 @@
+//
+//  NightClub.swift
+//  02-02 Syntax Nightclub
+//
+//  Created by Markus Wirtz on 01.07.24.
+//
 
+import Foundation
+
+// 1.2. NightClub Status
+
+if isOpen {
+    
+    if aktGuests <= maxGuests {
+        print("Herzlich willkommen im \(clubName).")
+        print("Treten Sie ein!")
+    }
+    else {
+        print("Der Club ist voll, bitte kommen Sie später wieder")
+    }
+}
+else {
+    print("Der Club ist aktuell geschlossen!")
+}
+
+// 1.3. Besucherdaten
+
+let guest1 = (Name: "Wolfgang Bauer", Alter: 61)
+let guest2 = (Name: "Maria Schmitt", Alter: 39)
+let guest3 = (Name: "Barbara Hintz", Alter: 57)
+let guest4 = (Name: "Jens Kohlmann", Alter: 54)
+
+// 1.4. Einlasskontrolle
+
+if guest1.Alter >= 18 {
+    // Alter ist OK
+    }
+else if (isOpen == true) && (aktGuests < maxGuests) {
+    print("Der Gast \(guest1.Name) darf in den Club")
+}
+else {
+    print("Der Gast \(guest1.Name) darf nicht in den Club")
+}
